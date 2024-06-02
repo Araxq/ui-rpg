5b70b398339e243f0f1c4e50e9c8f483829c821d
araxq <vlkachkovskiy@gmail.com>
2024-06-02 17:48:18 +0300

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigSolid from "eslint-plugin-solid/configs/typescript";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigSolid,
+  eslintConfigPrettier,
+  { rules: {} },
+];
---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+// import devtools from 'solid-devtools/vite';
+
+export default defineConfig({
+  plugins: [
+    /* 
+    Uncomment the following line to enable solid-devtools.
+    For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
+    */
+    // devtools(),
+    solidPlugin({
+      babel: {
+        plugins: [
+          [
+            "@locator/babel-jsx/dist",
+            {
+              env: "development",
+            },
+          ],
+        ],
+      },
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: "esnext",
+  },
+});
